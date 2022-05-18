cdaf087aceca5aff4c3a9eb4b4ad2a69ba0b8e7e
Murilo Alves <murilo.melo72@hotmail.com>
2022-05-18 00:23:53 -0300

    R104/endpoint_lattes_docente_GET

--- lattesDocente/lattesDocente_rotas.py
+++ lattesDocente/lattesDocente_rotas.py
@@ -21,11 +21,3 @@ def latte():
         lattes_modelo.append(LattesGetDocenteModelo(id= lattes.id, nome= lattes.nome, lattes= lattes.lattes))
     return lattes_modelo
 
-@router.post('/lattesDocente', response_model=LattesPostDocenteModelo)
-def latte(response: dict = Body(...)):
-    lattes_db = lattesDocenteDB().select().where((lattesDocenteDB.nome == response['nome'])&(lattesDocenteDB.lattes == response['lattes']))
-
-    if lattes_db.exists():
-        return LattesPostDocenteModelo(id = lattes_db.first().id, status = False)
-    id_temp = lattesDocenteDB().insert(nome = response['nome'], lattes = response['lattes']).execute()
-    return LattesPostDocenteModelo(id = id_temp, status= True)
